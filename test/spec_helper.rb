64c49d73c5ea350bbd8112c63ba628a19f3a96e8
Ben Abrams <me@benabrams.it>
2017-11-03 18:32:19 -0700

    revamp testing to be actual integration testing

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -5,11 +5,3 @@ RSpec.configure do |c|
   c.formatter = :documentation
   c.color = true
 end
-
-def fixture_path
-  File.expand_path('../fixtures', __FILE__)
-end
-
-def fixture(f)
-  File.new(File.join(fixture_path, f))
-end
